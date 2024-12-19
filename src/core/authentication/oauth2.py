ea12117d67d4f33be6f969bc9cdd2ad81895399b
Sangmin Lee <sangmin5848@naver.com>
2024-12-19 17:25:46 +0900

    Update oauth2.py

--- src/core/authentication/oauth2.py
+++ src/core/authentication/oauth2.py
@@ -2,7 +2,7 @@ from authlib.integrations.starlette_client import OAuth
 from starlette.config import Config
 
 
-class VdreamOAuth(OAuth):
+class OAuth(OAuth):
     name = 'google'
     server_metadata_url = 'https://accounts.google.com/.well-known/openid-configuration'
     client_kwargs = {'scope': 'openid profile email'}
